7f16d30daca7e4992b7ba4cf639d26e01b104381
Caroline LaDouce <39711539+carolineladouce@users.noreply.github.com>
2022-01-29 23:53:28 -0500

    Drew color gradient block on top of white and gray rounded rectangles

--- ReflectiveCard/CardView.swift
+++ ReflectiveCard/CardView.swift
@@ -62,7 +62,35 @@ class CardView: UIView {
         
         UIColor.white.setFill()
         innerRectPath.fill()
-   
+        
+        
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        
+        let color1 = UIColor.systemPurple.withAlphaComponent(0.75).cgColor
+        guard let color1Components = color1.components else { return }
+        let color2 = UIColor.systemTeal.withAlphaComponent(0.75).cgColor
+        guard let color2components = color2.components else { return }
+        
+        let colorComponents: [CGFloat] = [
+            color1Components[0],
+            color1Components[1],
+            color1Components[2],
+            color1Components[3],
+            color2components[0],
+            color2components[1],
+            color2components[2],
+            color2components[3]
+        ]
+        
+        let gradientColorLocations: [CGFloat] = [0.0, 1.0]
+        let gradientStartPoint = CGPoint(x: 0, y: 0)
+        let gradientEndPoint = CGPoint(x: 337, y: 212.5)
+        
+        guard let colorGradient = CGGradient(colorSpace: colorSpace, colorComponents: colorComponents, locations: gradientColorLocations, count: 2) else { return }
+        
+        context.drawLinearGradient(colorGradient, start: gradientStartPoint, end: gradientEndPoint, options: CGGradientDrawingOptions(rawValue: UInt32(0)))
+       
+        
         context.restoreGState()
     }
     
