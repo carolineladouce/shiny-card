520584debc38402ecdefc387d65eb6cd63ce98aa
Caroline LaDouce <39711539+carolineladouce@users.noreply.github.com>
2022-02-02 13:37:44 -0500

    Created new file TestShinyView to work out the shiny/reflective drawing feature

---
+++ ReflectiveCard/TestShinyView.swift
@@ -0,0 +1,20 @@
+//
+//  TestShinyView.swift
+//  ReflectiveCard
+//
+//  Created by Caroline LaDouce on 2/2/22.
+//
+
+import UIKit
+
+class TestShinyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
